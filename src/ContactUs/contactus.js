4f4e0e21e1fa1e6f3bd15e42b953ecb484d115c5
meerawadher <wadher.meera@gmail.com>
2021-08-18 23:15:27 +0530

    Initial Commits

---
+++ src/ContactUs/contactus.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+import { NetlifyForm, Honeypot } from 'react-netlify-forms'
+
+const ContactUs = () => (
+  <NetlifyForm name='Contact' action='/submit/' honeypotName='bot-field'>
+    {({ handleChange, success, error }) => (
+      <>
+        <Honeypot />
+        {success && <p>Thanks for contacting us!</p>}
+        {error && (
+          <p>Sorry, we could not reach our servers. Please try again later.</p>
+        )}
+        <div>
+          <label htmlFor='name'>Name:</label>
+          <input type='text' name='name' id='name' onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor='message'>Message:</label>
+          <textarea
+            type='text'
+            name='message'
+            id='message'
+            rows='4'
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <button type='submit'>Submit</button>
+        </div>
+      </>
+    )}
+  </NetlifyForm>
+)
+
+export default ContactUs;
+
---
+++ src/ContactUs/submit.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Submit = () => {
+    return <div >
+<p> Thank You for Your Submission. </p>
+        </div>
+}
+
+export default Submit;
