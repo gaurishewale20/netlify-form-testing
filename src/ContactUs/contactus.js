907ba8ab754b2975f3253488377b4012aa64957e
gaurishewale <gaurishewale201201@gmail.com>
2021-08-19 14:37:49 +0530

    Slight changes

--- src/ContactUs/contactus.js
+++ src/ContactUs/contactus.js
@@ -32,34 +32,26 @@ const ContactUs = () => (
   //   )}
   // </NetlifyForm>
   <center>
-  <form name="contact" method="POST" data-netlify="true">
-    <div class="fields">
-    <div class="field half">
-      <input type="text" name="name" id="name" placeholder="Your Name"/>
-    </div>
-    <br/>
-    <div class="field half">
-      <input type="email" name="email" id="email" placeholder="Email" />
-    </div>
-    <br/>
-    <div class="field">
-      <textarea name="message" id="message" rows="7" placeholder="Message"></textarea>
-    </div>
-    <br/>
-    <div class="field">
-      <input type="file" id="myfile" name="myfile" placeholder="Upload file"/>
-    </div>
-    <br/>
-    <div class="field">
-      <div>
-        <div data-netlify-recaptcha="true"></div>
+    <form name="contact-form" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="contact-form"/>
+      <div class="fields">
+        <div class="field half">
+          <input type="text" name="name" id="name" placeholder="Your Name" />
+        </div>
+        <br />
+        <div class="field half">
+          <input type="email" name="email" id="email" placeholder="Email" />
+        </div>
+        <br />
+        <div class="field">
+          <textarea name="message" id="message" rows="7" placeholder="Message"></textarea>
+        </div>
+        <br />
+        <div class="field">
+          <button type="submit">Send</button>
+        </div>
       </div>
-    </div>
-    <div class="field">
-      <button type="submit">Send</button>
-    </div>
-    </div>
-  </form>
+    </form>
   </center>
 )
 
