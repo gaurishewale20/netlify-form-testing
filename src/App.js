1db41966f0045f3bef4f79567e16ee68af82d88f
gaurishewale <gaurishewale201201@gmail.com>
2021-08-19 15:19:37 +0530

    Acknowledgement page added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ContactUs from "./ContactUs/contactus";
+import Submit from "./ContactUs/submit";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={ContactUs} />
+        <Route path="/formsubmitted" exact component={Submit} />
       </Switch>
     </BrowserRouter>
   );
